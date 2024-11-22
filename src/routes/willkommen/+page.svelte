291579f89e6ed39a4e808ede8936c52d8c16c18d
jona8175 <jonas.kubik@uzh.ch>
2024-11-22 16:21:38 +0100

    Test added

--- src/routes/willkommen/+page.svelte
+++ src/routes/willkommen/+page.svelte
@@ -3,6 +3,6 @@
 <body>
     
     <h1>Wilkommen zur Formenschatztruhe</h1>
-    <p>Auf dieser Website werden geologische und geomorphologische 3D Modele präsentiert.</p>
+    <p>Auf dieser Website werden geologische und geomorphologische 3D Modele präsentiert. Test</p>
     
 </body>
