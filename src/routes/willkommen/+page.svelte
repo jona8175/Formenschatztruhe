1cd3a01cb1289fc4bd1c26670d07b255dffef7f5
jona8175 <jonas.kubik@uzh.ch>
2024-11-24 16:02:24 +0100

    Merge test 2

--- src/routes/willkommen/+page.svelte
+++ src/routes/willkommen/+page.svelte
@@ -5,6 +5,6 @@
     <h1>Wilkommen zur Formenschatztruhe</h1>
     <p>Auf dieser Website werden geologische und geomorphologische 3D Modele präsentiert. </p>
     <br>
-    <p>Test to Merge</p>
+    <p>Test to Merge 2</p>
     
 </body>
